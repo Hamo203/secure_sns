46769839ae0126c398b326e678096d60cca1f840
羽持　涼花 <a5822097@aoyama.jp>
2025-01-11 23:58:06 +0900

    感情とimaagepathを結び付けている

---
+++ lib/view/components/emotionlists.dart
@@ -0,0 +1,19 @@
+import '../../model/emotionItem.dart';
+
+final List<EmotionItem> emotionItems = [
+  EmotionItem(name: "うれしい", imagePath: 'images/catface/ureshii.png'),
+  EmotionItem(name: "おもしろい", imagePath: 'images/catface/omoshiroi.png'),
+  EmotionItem(name: "わくわく", imagePath: 'images/catface/wakuwaku.png'),
+  EmotionItem(name: "どきどき", imagePath: 'images/catface/dokidoki.png'),
+  EmotionItem(name: "たのしい", imagePath: 'images/catface/tanoshi.png'),
+  EmotionItem(name: "あんしん", imagePath: 'images/catface/anshin.png'),
+  EmotionItem(name: "びっくり", imagePath: 'images/catface/bikkuri.png'),
+  EmotionItem(name: "かなしい", imagePath: 'images/catface/kanashi.png'),
+  EmotionItem(name: "いらいら", imagePath: 'images/catface/iraira.png'),
+  EmotionItem(name: "いや", imagePath: 'images/catface/iya.png'),
+  EmotionItem(name: "さみしい", imagePath: 'images/catface/samishi.png'),
+  EmotionItem(name: "こわい", imagePath: 'images/catface/kowai.png'),
+  EmotionItem(name: "つかれた", imagePath: 'images/catface/tsukareta.png'),
+  EmotionItem(name: "おちこむ", imagePath: 'images/catface/ochikomu.png')
+];
+
